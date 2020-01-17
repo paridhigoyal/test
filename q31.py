05bf315febf1244bbecf131c8f1a0fdb18a371dc
Paridhi <pgoyal@bestpeers.com>
2020-01-17 14:41:20 +0530

    question31

---
+++ q31.py
@@ -0,0 +1,12 @@
+def length_cal(s1,s2):
+    if(len(s1)==len(s2)):
+        print(s1)
+        print(s2)
+    if len(s1)>len(s2):
+        print(s1)
+    if len(s2)>len(s1):
+        print(s2)
+s1=input()
+s2=input()
+length_cal(s1,s2)
+
